99be6f0244f6a8715e4b0d58e652ac2999867c4f
mintun-myo-2020 <89120804+mintun-myo-2020@users.noreply.github.com>
2023-05-20 16:39:28 +0800

    first commit

---
+++ client/src/components/card/Cross.tsx
@@ -0,0 +1,29 @@
+import { IPositionData } from "../../interfaces/interfaces";
+
+interface CrossProps {
+  onMouseUp?: (event: React.MouseEvent<SVGSVGElement>) => void;
+  onMouseDown?: (event: React.MouseEvent<SVGSVGElement>) => void;
+}
+
+const Cross = ({ onMouseUp, onMouseDown }: CrossProps) => {
+  return (
+    <svg
+      onMouseUp={onMouseUp}
+      onMouseDown={onMouseDown}
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="5 -5 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className=" h-6 float-right hover:cursor-pointer transition duration-150 ease-in-out hover:text-yellow-100"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M6 18L18 6M6 6l12 12"
+      />
+    </svg>
+  );
+};
+
+export default Cross;
---
+++ client/src/interfaces/interfaces.ts
@@ -0,0 +1,19 @@
+export interface IPositionData {
+  x: number;
+  y: number;
+}
+
+export interface IQuestion {
+  question: string;
+}
+
+export interface IAnswer {
+  answer: string;
+}
+
+export interface ICardData  {
+  _id?: string;
+  question: IQuestion;
+  answer: IAnswer;
+  position: IPositionData;
+};
