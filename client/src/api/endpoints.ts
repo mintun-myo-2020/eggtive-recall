a7c31ae4d4878b29e28999dbcf4fe67ee7b75acf
mintun-myo-2020 <89120804+mintun-myo-2020@users.noreply.github.com>
2023-06-04 15:44:00 +0800

    slash at the end

--- client/src/api/endpoints.ts
+++ client/src/api/endpoints.ts
@@ -3,7 +3,7 @@ export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 
 export const API_ENDPOINTS = {
-  CARDS: "/card/",
-  USERS: "/user/",
+  CARDS: "card/",
+  USERS: "user/",
   OTHERS: "",
 };
