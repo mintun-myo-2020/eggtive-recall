8a1581f3057da22ee3cd88d26e60f5b28ba0552a
mintun-myo-2020 <89120804+mintun-myo-2020@users.noreply.github.com>
2023-06-18 13:24:16 +0800

    change background color

--- client/src/components/users/Register.tsx
+++ client/src/components/users/Register.tsx
@@ -23,7 +23,7 @@ const Register = () => {
   };
 
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex bg-offwhite justify-center items-center h-screen">
       <div className="max-w-sm mx-auto p-6 bg-white rounded shadow">
         <h1 className="text-center font-roboto text-3xl mb-3">Register</h1>
         <input
@@ -57,7 +57,7 @@ const Register = () => {
           <p className="text-center text-gray-500 text-sm">
             Already have an account? <span> </span>
             <Link to="/login" className="text-blue-500">
-              Log in Here
+              Login Here
             </Link>
           </p>
         </div>
