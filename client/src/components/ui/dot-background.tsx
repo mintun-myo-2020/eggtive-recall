84e3d09e0a4aeae775a93000514f5436486d9066
mintun-myo-2020 <89120804+mintun-myo-2020@users.noreply.github.com>
2024-02-28 16:55:02 +0800

    update about background

---
+++ client/src/components/ui/dot-background.tsx
@@ -0,0 +1,15 @@
+type DotBackgroundProps = {
+  contents: JSX.Element;
+};
+
+export function DotBackground({
+  contents,
+}: DotBackgroundProps): React.ReactElement<DotBackgroundProps> {
+  return (
+    <div className="h-[50rem] w-full dark:bg-black bg-white dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex items-center justify-center">
+      <div className="absolute pointer-events-none inset-0 flex items-center justify-center [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]">
+      </div>
+      {contents}
+    </div>
+  );
+}
--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -1,25 +1,26 @@
-import React from "react";
 import { Link } from "react-router-dom";
-import About from "../about/About";
 import { TextGenerateEffect } from "../../components/ui/text-generate-effect";
+import { DotBackground } from "../../components/ui/dot-background";
 
 const Home = () => {
+  const homeText =
+    "Start using proven techniques like active recall with eggtive today!";
 
-  const homeText = "Start using proven techniques like active recall with eggtive today!";
-
-  return (
-    <div className="bg-bgGray min-h-screen flex flex-col justify-center items-center font-oxygen">
-      <div className="max-w-md mx-auto p-6 bg-gray-100 rounded-lg shadow-lg">
-        <h1 className="text-3xl font-bold mb-4">Welcome to Eggtive</h1>
-        <p className="mb-8">
-          <TextGenerateEffect words={homeText} className="text-lg text-gray-600 " />
-        </p>
-        <Link to={"about"} className="pageBtn">
-          Learn More
-        </Link>
-      </div>
+  const contents: JSX.Element = (
+    <div className="max-w-md mx-auto p-6 bg-gray-100 rounded-lg shadow-lg">
+      <h1 className="text-3xl font-bold mb-4">Welcome to Eggtive</h1>
+      <p className="mb-8">
+        <TextGenerateEffect
+          words={homeText}
+          className="text-lg text-gray-600 "
+        />
+      </p>
+      <Link to={"about"} className="pageBtn">
+        Learn More
+      </Link>
     </div>
   );
+  return <DotBackground contents={contents} />;
 };
 
 export default Home;
