4c55ce04a76622b58bfccb502d2dace464caae3f
mintun-myo-2020 <89120804+mintun-myo-2020@users.noreply.github.com>
2024-03-31 17:55:03 +0800

    generate quiz with prompt using typechat

--- serverless/src/generate_quiz/handler.ts
+++ serverless/src/generate_quiz/handler.ts
@@ -1,49 +1,44 @@
 import { APIGatewayProxyEventV2, Context, Handler } from "aws-lambda";
-import * as typechat from "typechat";
 import { QuizSchema } from "./quizSchema";
 import path from "path";
 import fs from "fs";
+import { createTypeScriptJsonValidator } from "typechat/ts";
+import {
+  createJsonTranslator,
+  createLanguageModel,
+  createOpenAILanguageModel,
+} from "typechat";
+import { processRequests } from "typechat/interactive";
 
 interface Body {
-    noteHtml: string;
+  noteHtml: string;
 }
 
-const model = typechat.createOpenAILanguageModel(
-    process.env.OPENAI_API_KEY || "",
-    "gpt-3.5-turbo",
-    "chat"
+const model = createOpenAILanguageModel(
+  process.env.OPENAI_API_KEY || "",
+  "gpt-3.5-turbo"
 );
-
 const schema = fs.readFileSync(path.join(__dirname, "quizSchema.ts"), "utf8");
-const translator = typechat.createJsonTranslator<QuizSchema>(model, schema);
+const validator = createTypeScriptJsonValidator<QuizSchema>(
+  schema,
+  "QuizSchema"
+);
+const translator = createJsonTranslator(model, validator);
 
+export const handler: Handler = async (
+  event: APIGatewayProxyEventV2,
+  context: Context
+) => {
+  const body: Body = JSON.parse(event.body || "{}");
+  const prompt = translator.createRequestPrompt(
+    "Generate a quiz with 5 questions in the JSON format. The quiz should be based only on the content of the text. There should be 5 questions in the quiz. The questions should be in a JSON list"
+  );
+  const result = await translator.translate(body.noteHtml);
 
-export const handler: Handler = async (event: APIGatewayProxyEventV2, context: Context) => {
-
-    const body: Body = JSON.parse(event.body || '{}');
-
-    const response = await openai.chat.completions.create({
-        model: "gpt-3.5-turbo",
-        messages: [
-            {
-                "role": "system",
-                "content": "You are a quiz generator that takes in a note and creates a quiz that consists of 5 questions. The questions should be in the JSON format with question (string), options (object with option a,b,c,d as keys) and correctOption. Only return with the json object, starting and ending the response with { and } respectively."
-            },
-        ],
-        temperature: 1,
-        max_tokens: 1000,
-        top_p: 1,
-        frequency_penalty: 0,
-        presence_penalty: 0,
-    });
-
-    const content = response.choices[0].message.content;
-    const quiz = content ? JSON.parse(content) : null;
-
-    return {
-        statusCode: 200,
-        body: JSON.stringify({
-            quiz: quiz,
-        }),
-    };
-}
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      quiz: result,
+    }),
+  };
+};
