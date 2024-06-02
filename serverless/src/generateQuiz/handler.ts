467dc32cc4d02bd496610895ea8cea1a20e28f5b
Myo Min Tun <mintun.myo.2020@scis.smu.edu.sg>
2024-06-03 01:13:48 +0800

    generate quiz without typechat

--- serverless/src/generateQuiz/handler.ts
+++ serverless/src/generateQuiz/handler.ts
@@ -1,44 +1,44 @@
-import { APIGatewayProxyEventV2, Context, Handler } from "aws-lambda";
-import { QuizSchema } from "./quizSchema";
-import path from "path";
-import fs from "fs";
-import { createTypeScriptJsonValidator } from "typechat/ts";
-import {
-  createJsonTranslator,
-  createLanguageModel,
-  createOpenAILanguageModel,
-} from "typechat";
-import { processRequests } from "typechat/interactive";
+import { Handler, APIGatewayProxyEventV2, Context } from "aws-lambda";
+import OpenAI from "openai";
+
 
 interface Body {
   noteHtml: string;
 }
 
-const model = createOpenAILanguageModel(
-  process.env.OPENAI_API_KEY || "",
-  "gpt-3.5-turbo"
-);
-const schema = fs.readFileSync(path.join(__dirname, "quizSchema.ts"), "utf8");
-const validator = createTypeScriptJsonValidator<QuizSchema>(
-  schema,
-  "QuizSchema"
-);
-const translator = createJsonTranslator(model, validator);
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY
+});
 
 export const handler: Handler = async (
   event: APIGatewayProxyEventV2,
   context: Context
 ) => {
   const body: Body = JSON.parse(event.body || "{}");
-  const prompt = translator.createRequestPrompt(
-    "Generate a quiz with 5 questions in the JSON format. The quiz should be based only on the content of the text. There should be 5 questions in the quiz. The questions should be in a JSON list"
-  );
-  const result = await translator.translate(body.noteHtml);
+
+  const chatCompletion = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    response_format: { "type": "json_object" },
+    messages: [
+      {
+        role: "system",
+        content: "You are a quiz generating expert that takes in a note and generates a quiz JSON object (main key is quiz) with the subkeys being question and answer.",
+      },
+      {
+        role: "user",
+        content: body.noteHtml,
+      },
+    ],
+  });
+
+
+  const quizContent = chatCompletion.choices[0].message.content || "{}";
+  const quiz = JSON.parse(quizContent).quiz;
 
   return {
     statusCode: 200,
     body: JSON.stringify({
-      quiz: result,
+      quiz,
     }),
   };
 };
