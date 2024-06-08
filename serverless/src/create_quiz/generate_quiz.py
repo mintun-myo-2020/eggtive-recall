a7a3717ab02aaa4c0dd1f76d570e2c3841c564bf
Myo Min Tun <mintun.myo.2020@scis.smu.edu.sg>
2024-06-09 00:45:53 +0800

    stop using typechat

--- serverless/src/create_quiz/generate_quiz.py
+++ serverless/src/create_quiz/generate_quiz.py
@@ -2,35 +2,28 @@ import json
 import os
 from openai import OpenAI
 
-import schema
-from typechat import create_openai_language_model, TypeChatJsonTranslator, TypeChatValidator
-
 from src.exceptions.quiz_empty_exception import QuizEmptyError
 
-openai_api_key = os.getenv("OPENAI_API_KEY")
-model = create_openai_language_model(openai_api_key, "gpt-3.5-turbo")
-validator = TypeChatValidator()
-translator = TypeChatJsonTranslator(model. validator, schema.QuizSchema)
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 
 def handler(event, context):
     note_html = event.get("noteHtml")
     user_id = event.get("userId")
-    translator.translate("You are a quiz generating expert that takes in a note and generates a quiz. This is the quiz in html form: " + note_html)
-    # chat_completion = client.chat.completions.create(
-    #     model="gpt-3.5-turbo",
-    #     response_format={"type": "json_object"},
-    #     messages=[
-    #         {
-    #             "role": "system",
-    #             "content": "You are a quiz generating expert that takes in a note and generates a quiz JSON object (main key is quiz) with the subkeys being question and answer.",
-    #         },
-    #         {
-    #             "role": "user",
-    #             "content": note_html,
-    #         },
-    #     ],
-    # )
+    chat_completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        response_format={"type": "json_object"},
+        messages=[
+            {
+                "role": "system",
+                "content": "You are a quiz generating expert that takes in a note and generates a quiz JSON object (main key is quiz) with the subkeys being question and answer.",
+            },
+            {
+                "role": "user",
+                "content": note_html,
+            },
+        ],
+    )
 
     try:
         quiz_content = chat_completion.choices[0].message.content
